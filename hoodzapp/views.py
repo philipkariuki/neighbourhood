63183e665a1afe70221c0057ef725aefcef333c9
philipkariuki <kariukiphil@gmail.com>
2019-08-13 00:14:51 +0300

    in views.py imports most of the required django modules
    creates forms.py in app

--- hoodzapp/views.py
+++ hoodzapp/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+from django.db import transaction
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 # Create your views here.
 def index(request):
